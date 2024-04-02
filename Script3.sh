389dc8114cd928a28845ad5d58b8f4c70c60293a
Pranil Patel <120561313+Pranil0712@users.noreply.github.com>
2024-04-02 16:29:12 +0530

    Deleted Script3

--- Script3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this is to check if the shell scripts picks up env variables
-echo "This is error script"
-echo "This is my system path $PATH"
-
-#Set a JDK_HOME env variable
-export JDK_HOME=/bin/jdk
-echo "my new JDK home is=$JDK_HOME"
